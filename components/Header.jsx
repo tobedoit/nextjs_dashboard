9207891713065da125ff238d1b3695d9569000f7
tobedoit <tobedoit@aol.com>
2023-03-12 09:56:27 +0900

    first commit

---
+++ components/Header.jsx
@@ -0,0 +1,11 @@
+
+const Header = () => {
+  return (
+    <div className="flex justify-between px-4 pt-4">
+      <h2>Dashboard</h2>
+      <h2>Welcome back, Zmes</h2>
+    </div>
+  )
+}
+
+export default Header
